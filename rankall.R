6dca98895339ad9b73c2c5c5a5f47e5d12002927
eugenedobro <evgenyi-dobrogodin@yandex.ru>
2015-02-27 17:22:06 +0300

    Complete 3rd task + testing file

--- rankall.R
+++ rankall.R
@@ -3,132 +3,60 @@ rankall <- function(outcome, num = "best") {
         df <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
         
         # outcome <- "heart attack" 
-        # diseas <- "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia"
+        # diseas <- "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack"
         #
         ## Check that state and outcome are valid
-        if (!(state %in% df[,7])) {
-                stop("invalid state")
-        } else if (!(outcome %in% c("heart attack", "heart failure", "pneumonia"))) {
+        if (!(outcome %in% c("heart attack", "heart failure", "pneumonia"))) {
                 stop("invalid outcome")
         } else {
                 
                 ## For each state, find the hospital of the given rank
                 ## Return a data frame with the hospital names and the
                 ## (abbreviated) state name
-                diseas <- if (outcome == "heart attack") {
-                        "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack"
+                if (outcome == "heart attack") {
+                        diseas <- "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack"
+                        n <- 11
                 } else if (outcome == "heart failure") {
-                        "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure"
+                        diseas <- "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure"
+                        n <- 17
                 } else {
-                        "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia"
+                        diseas <- "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia"
+                        n <- 23
                 }
                 
                 ## New data frame withot omitted data
                 df1 <- data.frame()
                 df1 <- df[complete.cases(suppressWarnings(as.numeric(df[,diseas]))),]
+                df1 <- data.frame()
                 
+                #2 name, 7 state, 23 pneumonia
+                df1 <- df[complete.cases(suppressWarnings(as.numeric(df[,diseas]))),c(2, 7, n)]
+                df1[,3] <- as.numeric(df1[,3])
+                df1[,2] <- as.factor(df1[,2])
+                
+                df1 <- df1[order(df1[,2],df1[,3],df1[,1]),]
                 ## Choose rows that respond to our state
                 ## Search for the hospital
-                if (num == "best"){
-                        df1 <- df1[which(as.numeric(df1[,diseas]) == min(as.numeric(df1[,diseas]))), ]
-                        res <- as.character(df1[,2])
-                } else if (num == "worst"){
-                        df1 <- df1[which(as.numeric(df1[,diseas]) == max(as.numeric(df1[,diseas]))), ]
-                        res <- as.character(df1[,2])
-                } else {
-                        df2 <- data.frame()
-                        df2 <- df1[order(as.numeric(df1[,diseas]), df1[,2]),] 
-                        res <- df2[num, 2]
+                l <- split(df1, df1[,2])
+                
+                result <- data.frame("Hospital Name"=character(0), "State"=character(0), "Diseas Mortality Level"=numeric(0))
+                #colnames(result) <- c("Hospital Name", "State", "Diseas Mortality Level") 
+                colnames(result) <- colnames(df1)
+                
+                for (i in 1:length(l)) {
+                        df_state <- as.data.frame(l[i][1])
+                        colnames(df_state) <- colnames(df1)
+                        if (num == "best") {
+                                result <- rbind(result, df_state[1,])                                
+                        } else if (num == "worst"){
+                                result <- rbind(result, df_state[length(df_state[,3]),])          
+                        } else {
+                                result <- rbind(result, df_state[num,])
+                        }
                 }
-                
-                
-                ##return the name of the numth best hospital
-                res        
+                colnames(result) <- c("hospital", "state", "Diseas Mortality Level")
+                result[,1:2]           
         }            
 }
 
-
-##########################
-df <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
-outcome <- "pneumonia" 
-diseas <- "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia"
-result <- data.frame()
-df1 <- data.frame()
-
-#2 name, 7 state, 23 pneumonia
-df1 <- df[complete.cases(suppressWarnings(as.numeric(df[,diseas]))),c(2, 7, 23)]
-df1[,3] <- as.numeric(df1[,3])
-df1[,2] <- as.factor(df1[,2])
-#df1 <- df1[order(df1[,1]),]
-#df1[,1]
-#head(df1)
-
-
-
-func <- function (num = "best") {
-        if (num == "best"){
-                df1 <- df1[which(df1[,diseas] == min(df1[,diseas])), ]
-                res <- as.character(df1[,2])
-        } else if (num == "worst"){
-                df1 <- df1[which(as.numeric(df1[,diseas]) == max(as.numeric(df1[,diseas]))), ]
-                res <- as.character(df1[,2])
-        } else {
-                df2 <- data.frame()
-                df2 <- df1[order(as.numeric(df1[,diseas]), df1[,2]),] 
-                res <- as.character(df2[num, 2])
-        }
-}
-
-
-
-##########################
-df <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
-outcome <- "pneumonia" 
-diseas <- "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia"
-num = "best"
-
-result <- data.frame()
-df1 <- data.frame()
-
-#2 name, 7 state, 23 pneumonia
-df1 <- df[complete.cases(suppressWarnings(as.numeric(df[,diseas]))),c(2, 7, 23)]
-df1[,3] <- as.numeric(df1[,3])
-df1[,2] <- as.factor(df1[,2])
-
-df1 <- df1[order(df1[,2],df1[,3],df1[,1]),]
-#head(df1[,3])
-str(df1)
-names(df1)[3]<-"Pneumonia"
-head(df1, 15)
-
-l <- split(df1, df1[,2])
-str(l)
-class(l[1][1])
-l[1][1]
-l[length(l)][1]
-result <- as.data.frame(l[1][1])
-result
-
-for (i in 1:lenght(l)) {
-        
-}
-
-res <- data.frame()
-#res <- do.call(rbind, by(df1, df1[,2], head, 1))
-#res
-rankCompany <- function(num) {
-        result <- data.frame()
-        if (num == "best") {
-                result <- do.call(rbind, by(df1, df1[,2], head, 1))
-        } else if (num == "worst") {
-                result <- do.call(rbind, by(df1, df1[,2], tail, 1))
-        } else {
-                result <- merge(result, data.frame(Category = df1[,2], Ranking = num), all.y = TRUE)
-        }
-        result
-}
-
-rankCompany(num = "worst")
-rankCompany(num = "best")
-
-
+head(rankall("heart attack", 20), 10)
